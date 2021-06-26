fa5b2b9a6bf5af015ca585de7de8659a24c934b0
ahkohd <victor.olorunbunmi@gmail.com>
2021-06-27 00:22:05 +0100

    wip: implement add new sprites

--- src/view/app/components/ControlPanel/ToolsPanel.tsx
+++ src/view/app/components/ControlPanel/ToolsPanel.tsx
@@ -1,6 +1,6 @@
 import Icon from 'components/Icon'
 import { WebViewMessage } from 'consts/message'
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import { vscode } from 'utils/vscode'
 import './style.scss'
 
@@ -12,14 +12,41 @@ const ToolsPanel = () => {
         })
     }
 
+    const handleAddNewSprites = async (
+        event: ChangeEvent<HTMLInputElement>
+    ) => {
+        const { files } = event.target
+        if (files) {
+            const svgs = await Promise.all(
+                Array.from(files).map((file) => file.text())
+            )
+
+            console.log(svgs, 'svgs')
+
+            vscode.postMessage({
+                command: WebViewMessage.addNewSprites,
+                svgs,
+            })
+        }
+    }
+
     return (
         <div className="tools_panel mb-15">
             <div className="tools_panel__actions">
                 <button className="btn--icon mr-5">
+                    <label htmlFor="add-sprites"></label>
+                    <input
+                        type="file"
+                        id="add-sprites"
+                        name="newSprites"
+                        accept="image/svg+xml"
+                        multiple
+                        onChange={handleAddNewSprites}
+                    />
                     <Icon id="add-file" size={18} />
                 </button>
                 <button className="btn--icon" onClick={handleRefresh}>
-                    <Icon id="refresh" size={18} />
+                    <Icon id="refresh" size={17} />
                 </button>
             </div>
         </div>
--- src/view/app/consts/message.ts
+++ src/view/app/consts/message.ts
@@ -1,4 +1,5 @@
 export enum WebViewMessage {
     alert,
     reload,
+    addNewSprites,
 }
