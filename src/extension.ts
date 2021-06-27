261255e00e2294c0ea590ccd5e0476488d2c296d
ahkohd <victor.olorunbunmi@gmail.com>
2021-06-27 15:20:01 +0100

    refactor: remove comment

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,6 @@
 import { ExtensionContext, commands } from 'vscode'
 import SvgSpritesViewer from './SvgSpritesViewer'
 
-// @TODO: Add action icon to the editor tabs bar, when
-// clicked it will open the SVG sprites view
-
 export function activate(context: ExtensionContext) {
     let disposable = commands.registerCommand('glance.open', () =>
         SvgSpritesViewer.instance.onActivate(context)
