bf78e2703ce768ed6e93088b2a354bf9cfe7a260
ahkohd <victor.olorunbunmi@gmail.com>
2021-06-27 15:12:56 +0100

    refactor: break into component

---
+++ src/view/app/components/ControlPanel/AppearanceControl.tsx
@@ -0,0 +1,91 @@
+import React, { ChangeEvent } from 'react'
+import useStore from 'store/store'
+
+const AppearanceControl = () => {
+    const [{ config }, actions] = useStore()
+
+    const handleChange = (
+        event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ) => {
+        const key = event.target.name
+
+        actions.configure({
+            [key]: event.target.value,
+        })
+    }
+
+    return (
+        <ul className="config_controls">
+            <li>
+                <label>Size</label>
+                <div className="control">
+                    <input
+                        type="number"
+                        className="input"
+                        placeholder={'Enter a size'}
+                        value={config.size}
+                        name="size"
+                        onChange={handleChange}
+                        min="1"
+                        max="100"
+                    />
+                </div>
+            </li>
+            <li>
+                <label>Color</label>
+                <div className="control">
+                    <input
+                        type="color"
+                        className="input"
+                        placeholder={config.color}
+                        value={config.color}
+                        name="color"
+                        onChange={handleChange}
+                    />
+                </div>
+            </li>
+            <li>
+                <label>Stroke</label>
+                <div className="control">
+                    <input
+                        type="color"
+                        className="input"
+                        placeholder={config.stroke}
+                        value={config.stroke}
+                        name="stroke"
+                        onChange={handleChange}
+                    />
+                </div>
+            </li>
+            <li>
+                <label className="mb-5">Stroke width</label>
+                <div className="control">
+                    <input
+                        type="number"
+                        className="input"
+                        placeholder={'Enter a stroke width'}
+                        value={config.strokeWidth}
+                        name="strokeWidth"
+                        onChange={handleChange}
+                        min="0"
+                    />
+                </div>
+            </li>
+            <li>
+                <label className="mb-5">Copy</label>
+                <div className="control">
+                    <select
+                        name="copyType"
+                        className="input"
+                        onChange={handleChange}
+                    >
+                        <option value="assetId">Asset's ID</option>
+                        <option value="svgCode">SVG Code</option>
+                    </select>
+                </div>
+            </li>
+        </ul>
+    )
+}
+
+export default AppearanceControl
--- src/view/app/components/ControlPanel/ControlPanel.tsx
+++ src/view/app/components/ControlPanel/ControlPanel.tsx
@@ -1,96 +1,15 @@
+import React from 'react'
 import Searchbar from 'components/Searchbar/Searchbar'
-import React, { ChangeEvent } from 'react'
-import useStore from 'store/store'
-import './style.scss'
 import ToolsPanel from './ToolsPanel'
+import AppearanceControl from './AppearanceControl'
+import './style.scss'
 
 const ControlPanel = () => {
-    const [{ config }, actions] = useStore()
-
-    const handleChange = (
-        event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-    ) => {
-        const key = event.target.name
-
-        actions.configure({
-            [key]: event.target.value,
-        })
-    }
-
     return (
         <aside className="control_panel">
             <ToolsPanel />
             <Searchbar />
-            <ul className="config_controls">
-                <li>
-                    <label>Size</label>
-                    <div className="control">
-                        <input
-                            type="number"
-                            className="input"
-                            placeholder={'Enter a size'}
-                            value={config.size}
-                            name="size"
-                            onChange={handleChange}
-                            min="1"
-                            max="100"
-                        />
-                    </div>
-                </li>
-                <li>
-                    <label>Color</label>
-                    <div className="control">
-                        <input
-                            type="color"
-                            className="input"
-                            placeholder={config.color}
-                            value={config.color}
-                            name="color"
-                            onChange={handleChange}
-                        />
-                    </div>
-                </li>
-                <li>
-                    <label>Stroke</label>
-                    <div className="control">
-                        <input
-                            type="color"
-                            className="input"
-                            placeholder={config.stroke}
-                            value={config.stroke}
-                            name="stroke"
-                            onChange={handleChange}
-                        />
-                    </div>
-                </li>
-                <li>
-                    <label className="mb-5">Stroke width</label>
-                    <div className="control">
-                        <input
-                            type="number"
-                            className="input"
-                            placeholder={'Enter a stroke width'}
-                            value={config.strokeWidth}
-                            name="strokeWidth"
-                            onChange={handleChange}
-                            min="0"
-                        />
-                    </div>
-                </li>
-                <li>
-                    <label className="mb-5">Copy</label>
-                    <div className="control">
-                        <select
-                            name="copyType"
-                            className="input"
-                            onChange={handleChange}
-                        >
-                            <option value="assetId">Asset's ID</option>
-                            <option value="svgCode">SVG Code</option>
-                        </select>
-                    </div>
-                </li>
-            </ul>
+            <AppearanceControl />
         </aside>
     )
 }
