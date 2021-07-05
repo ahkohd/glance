9289019c1d26604fc9a14b770b9c14f031d9dcc9
ahkohd <victor.olorunbunmi@gmail.com>
2021-07-05 04:06:47 +0100

    wip: rename and delete sprite

--- src/view/app/components/Grid/GridItem.tsx
+++ src/view/app/components/Grid/GridItem.tsx
@@ -1,9 +1,16 @@
 import { WebViewMessage } from 'consts/message'
-import React, { useMemo } from 'react'
+import React, {
+    ChangeEvent,
+    MouseEvent,
+    useMemo,
+    useRef,
+    useState,
+} from 'react'
 import useStore from 'store/store'
 import { copyToClipboard } from 'utils/clipboard'
 import { vscode } from 'utils/vscode'
 import { renderToString } from 'react-dom/server'
+import Icon from 'components/Icon'
 
 export interface SVGRecord {
     id: string
@@ -18,6 +25,9 @@ interface GridItemProps {
 const GridItem = (props: GridItemProps): JSX.Element => {
     const [{ query, config }] = useStore()
     const { svg } = props
+    const [editMode, setEditMode] = useState(false)
+    const [spriteName, setSpriteName] = useState(svg.id)
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const getSize = (value: string | undefined) => {
         const size = parseInt(value ?? '0')
@@ -43,6 +53,27 @@ const GridItem = (props: GridItemProps): JSX.Element => {
         })
     }
 
+    const handleToggleEditMode = (event: MouseEvent<HTMLButtonElement>) => {
+        event.stopPropagation()
+
+        setEditMode((prev) => {
+            const next = !prev
+            if (next) {
+                inputRef.current!.focus()
+            }
+
+            return next
+        })
+    }
+
+    const handleOnNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSpriteName(event.target.value)
+    }
+
+    const handleDeleteSprite = (event: MouseEvent<HTMLButtonElement>) => {
+        event.stopPropagation()
+    }
+
     const Svg = useMemo(
         () => (
             <svg
@@ -64,7 +95,26 @@ const GridItem = (props: GridItemProps): JSX.Element => {
     return (
         <a href="#" className="svg_grid__preview" onClick={handleClick}>
             {Svg}
-            <p className="svg_grid__label mt-5">{svg.id}</p>
+            <input
+                readOnly={!editMode}
+                type="text"
+                className="svg_grid__label mt-5"
+                value={spriteName}
+                ref={inputRef}
+                onChange={handleOnNameChange}
+            />
+            <div className="svg_grid__actions">
+                <button className="btn--icon" onClick={handleToggleEditMode}>
+                    <Icon
+                        id={editMode ? 'save' : 'edit'}
+                        width="16px"
+                        height="16px"
+                    />
+                </button>
+                <button className="btn--icon" onClick={handleDeleteSprite}>
+                    <Icon id="trash" width="16px" height="16px" />
+                </button>
+            </div>
         </a>
     )
 }
